33c0a7ffa0ba3cc857c75c7394d9f7c7cc590e26
Adrian-Nicolae Dediu <111912660+adrdediu@users.noreply.github.com>
2023-01-27 14:00:19 +0200

    Modified the Readme file and included the amplify folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frontendjourneymetacacc77d30": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
